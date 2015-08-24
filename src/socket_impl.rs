5c5965dd78c8980d6fe5488d9454c7878a03fbf8
blabaere <benoit.labaere@gmail.com>
2015-08-24 21:44:48 +0200

    Adds a license

--- src/socket_impl.rs
+++ src/socket_impl.rs
@@ -1,6 +1,6 @@
 // Copyright 2015 Copyright (c) 2015 Benoît Labaere (benoit.labaere@gmail.com)
 //
-// Licensed under  the MIT license LICENSE or <http://opensource.org/licenses/MIT>
+// Licensed under the MIT license LICENSE or <http://opensource.org/licenses/MIT>
 // This file may not be copied, modified, or distributed except according to those terms.
 
 use std::rc::Rc;
