ac499f687ceae594d4494cfd2e99c05b11a3359c
blabaere <benoit.labaere@gmail.com>
2015-09-11 22:27:46 +0200

    Refactors Resp to use sender and receiver

--- src/protocol/resp.rs
+++ src/protocol/resp.rs
@@ -6,7 +6,6 @@
 use std::rc::Rc;
 use std::sync::mpsc::Sender;
 use std::io;
-use std::boxed::FnBox;
 
 use mio;
 
@@ -21,182 +20,24 @@ use EventLoop;
 use EventLoopAction;
 use Message;
 
+use super::sender::*;
+use super::receiver::*;
+
 pub struct Resp {
     pipe: Option<Pipe>,
-    evt_sender: Rc<Sender<SocketEvt>>,
-    cancel_send_timeout: Option<EventLoopAction>,
-    cancel_recv_timeout: Option<EventLoopAction>,
-    pending_send: Option<Rc<Message>>,
-    pending_recv: bool,
-    backtrace: Vec<u8>,
-    ttl: u8
+    msg_sender: UnaryMsgSender,
+    msg_receiver: UnaryMsgReceiver,
+    codec: Codec
 }
 
 impl Resp {
-    pub fn new(evt_sender: Rc<Sender<SocketEvt>>) -> Resp {
+    pub fn new(evt_tx: Rc<Sender<SocketEvt>>) -> Resp {
         Resp { 
             pipe: None,
-            evt_sender: evt_sender,
-            cancel_send_timeout: None,
-            cancel_recv_timeout: None,
-            pending_send: None,
-            pending_recv: false,
-            backtrace: Vec::with_capacity(64),
-            ttl: 8
+            msg_sender: UnaryMsgSender::new(evt_tx.clone()),
+            msg_receiver: UnaryMsgReceiver::new(evt_tx.clone()),
+            codec: Codec::new()
         }
-    }
-
-    fn on_msg_send_finished(&mut self, event_loop: &mut EventLoop, evt: SocketEvt) {
-        let _ = self.evt_sender.send(evt);
-        let timeout = self.cancel_send_timeout.take();
-
-        timeout.map(|cancel_timeout| cancel_timeout.call_box((event_loop,)));
-
-        self.pending_send = None;
-    }
-
-    fn on_msg_send_finished_ok(&mut self, event_loop: &mut EventLoop) {
-        self.on_msg_send_finished(event_loop, SocketEvt::MsgSent);
-
-        if let Some(pipe) = self.pipe.as_mut() {
-            pipe.finish_send(); 
-        }
-    }
-
-    fn on_msg_send_finished_err(&mut self, event_loop: &mut EventLoop, err: io::Error) {
-        self.on_msg_send_finished(event_loop, SocketEvt::MsgNotSent(err));
-
-        if let Some(pipe) = self.pipe.as_mut() {
-            pipe.cancel_send(); 
-        }
-    }
-
-    fn on_msg_send_started(&mut self) {
-        self.pending_send = None;
-    }
-
-    fn process_send_result(&mut self, event_loop: &mut EventLoop, sent: bool, sending: bool) {
-        if sent {
-            self.on_msg_send_finished_ok(event_loop);
-        } else if sending {
-            self.on_msg_send_started();
-        }
-    }
-
-    fn on_msg_recv_finished(&mut self, event_loop: &mut EventLoop, evt: SocketEvt) {
-        let _ = self.evt_sender.send(evt);
-        let timeout = self.cancel_recv_timeout.take();
-
-        timeout.map(|cancel_timeout| cancel_timeout.call_box((event_loop,)));
-
-        self.pending_recv = false;
-    }
-
-    fn on_msg_recv_finished_ok(&mut self, event_loop: &mut EventLoop, msg: Message) {
-        self.save_received_header_to_backtrace(&msg);
-        self.on_msg_recv_finished(event_loop, SocketEvt::MsgRecv(msg));
-
-        if let Some(pipe) = self.pipe.as_mut() {
-            pipe.finish_recv(); 
-        }
-    }
-
-    fn on_msg_recv_finished_err(&mut self, event_loop: &mut EventLoop, err: io::Error) {
-        self.on_msg_recv_finished(event_loop, SocketEvt::MsgNotRecv(err));
-
-        if let Some(pipe) = self.pipe.as_mut() {
-            pipe.cancel_recv(); 
-        }
-    }
-
-    fn on_msg_recv_started(&mut self, _: mio::Token) {
-        self.pending_recv = false;
-    }
-
-
-    fn process_recv_result(
-        &mut self, 
-        event_loop: &mut EventLoop, 
-        received: Option<(Message, mio::Token)>, 
-        receiving: Option<mio::Token>) {
-
-        if let Some((raw_msg, from)) = received {
-            self.on_raw_msg_recv_from(event_loop, raw_msg, from);
-        }
-        else if let Some(token) = receiving {
-            self.on_msg_recv_started(token);
-        }
-    }
-
-    fn on_raw_msg_recv_from(&mut self, event_loop: &mut EventLoop, raw_msg: Message, pipe_token: mio::Token) {
-        match self.raw_msg_to_msg(pipe_token, raw_msg) {
-            Ok(None)      => {}
-            Ok(Some(msg)) => self.on_msg_recv_finished_ok(event_loop, msg),
-            Err(e)        => self.on_msg_recv_finished_err(event_loop, e)
-        }
-    }
-
-    fn save_received_header_to_backtrace(&mut self, msg: &Message) {
-        self.backtrace.clear();
-        self.backtrace.push_all(msg.get_header());
-    }
-
-    fn raw_msg_to_msg(&mut self, pipe_token: mio::Token, raw_msg: Message) -> io::Result<Option<Message>> {
-        let (mut header, mut body) = raw_msg.explode();
-        let pipe_id = pipe_token.as_usize() as u32;
-
-        header.reserve(4);
-        try!(header.write_u32::<BigEndian>(pipe_id));
-
-        let mut hops = 0;
-        loop {
-            if hops >= self.ttl {
-                return Ok(None);
-            }
-
-            hops += 1;
-
-            if body.len() < 4 {
-                return Ok(None);
-            }
-
-            let tail = body.split_off(4);
-            header.push_all(&body);
-
-            let position = header.len() - 4;
-            if header[position] & 0x80 != 0 {
-                let msg = Message::with_header_and_body(header, tail);
-
-                return Ok(Some(msg));
-            }
-            body = tail;
-        }
-
-    }
-
-    fn msg_to_raw_msg(&mut self, msg: Message) -> io::Result<(Message, mio::Token)> {
-        let (mut header, body) = msg.explode();
-        let pipe_id_bytes = vec!(
-            self.backtrace[0],
-            self.backtrace[1],
-            self.backtrace[2],
-            self.backtrace[3]
-        );
-        let mut pipe_id_reader = io::Cursor::new(pipe_id_bytes);
-        let pipe_id = try!(pipe_id_reader.read_u32::<BigEndian>());
-        let pipe_token = mio::Token(pipe_id as usize);
-
-        self.restore_saved_backtrace_to_header(&mut header);
-        self.backtrace.clear();
-
-        Ok((Message::with_header_and_body(header, body), pipe_token))
-    }
-
-    fn restore_saved_backtrace_to_header(&self, header: &mut Vec<u8>) {
-        let backtrace = &self.backtrace[4..];
-        
-        header.reserve(backtrace.len());
-        header.push_all(backtrace);
     }
 }
 
@@ -222,119 +63,124 @@ impl Protocol for Resp {
     }
 
     fn send(&mut self, event_loop: &mut EventLoop, msg: Message, cancel_timeout: EventLoopAction) {
-        self.cancel_send_timeout = Some(cancel_timeout);
-
-        if self.pipe.is_none() {
-            self.pending_send = Some(Rc::new(msg));
-            return;
+        match self.codec.encode(msg) {
+            Err(e) => self.msg_sender.on_send_err(event_loop, e, self.pipe.as_mut()),
+            Ok((raw_msg, _)) => self.msg_sender.send(event_loop, raw_msg, cancel_timeout, self.pipe.as_mut())
         }
-
-        let (raw_msg, pipe_token) = match self.msg_to_raw_msg(msg) {
-            Err(e) => return self.on_msg_send_finished_err(event_loop, e),
-            Ok((raw_msg, pipe_token)) => (raw_msg, pipe_token)
-        };
-
-        let mut pipe = self.pipe.take().unwrap();
-        let mut sent = false;
-        let mut sending = false;
-        let msg = Rc::new(raw_msg);
-
-        if pipe_token != pipe.token() {
-            let err = io::Error::new(io::ErrorKind::NotConnected, "original surveyor disconnected");
-
-            self.on_msg_send_finished_err(event_loop, err);
-            self.pipe = Some(pipe);
-
-            return;
-        }
-
-        match pipe.send(msg.clone()) {
-            Ok(SendStatus::Completed)  => sent = true,
-            Ok(SendStatus::InProgress) => sending = true,
-            _ => {}
-        }
-
-        self.pipe = Some(pipe);
-        self.pending_send = Some(msg);
-        self.process_send_result(event_loop, sent, sending);
     }
 
     fn on_send_timeout(&mut self, event_loop: &mut EventLoop) {
-        let err = io::Error::new(io::ErrorKind::TimedOut, "send timeout reached");
-
-        self.on_msg_send_finished_err(event_loop, err);
+        self.msg_sender.on_send_timeout(event_loop, self.pipe.as_mut())
     }
 
     fn recv(&mut self, event_loop: &mut EventLoop, cancel_timeout: EventLoopAction) {
-        self.cancel_recv_timeout = Some(cancel_timeout);
-
-        if self.pipe.is_none() {
-            self.pending_recv = true;
-            return;
-        }
-
-        let mut pipe = self.pipe.take().unwrap();
-        let mut received = None;
-        let mut receiving = None;
-
-        match pipe.recv() {
-            Ok(RecvStatus::Completed(msg)) => received = Some((msg, pipe.token())),
-            Ok(RecvStatus::InProgress)     => receiving = Some(pipe.token()),
-            _ => {}
-        }
-
-        self.pipe = Some(pipe);
-        self.pending_recv = true;
-        self.process_recv_result(event_loop, received, receiving);
+        self.msg_receiver.recv(event_loop, &mut self.codec, cancel_timeout, self.pipe.as_mut())
     }
-
+    
     fn on_recv_timeout(&mut self, event_loop: &mut EventLoop) {
-        let err = io::Error::new(io::ErrorKind::TimedOut, "recv timeout reached");
-
-        self.on_msg_recv_finished_err(event_loop, err);
+        self.msg_receiver.on_recv_timeout(event_loop, self.pipe.as_mut())
     }
 
     fn ready(&mut self, event_loop: &mut EventLoop, token: mio::Token, events: mio::EventSet) -> io::Result<()> {
-        if self.pipe.is_none() {
-            return Ok(());
-        }
-        if Some(token) != self.pipe.as_ref().map(|p| p.token()) {
-            return Ok(());
-        }
         let mut sent = false;
-        let mut sending = false;
         let mut received = None;
-        let mut receiving = None;
 
         if let Some(pipe) = self.pipe.as_mut() {
-            let has_pending_send = self.pending_send.is_some();
-            let has_pending_recv = self.pending_recv;
             let (s, r) = try!(pipe.ready(event_loop, events));
             sent = s;
-            received = r.map(|msg| (msg, pipe.token()));
-
-            if has_pending_send && !sent && pipe.can_resume_send() {
-                let msg = self.pending_send.as_ref().unwrap();
-
-                match try!(pipe.send(msg.clone())) {
-                    SendStatus::Completed  => sent = true,
-                    SendStatus::InProgress => sending = true,
-                    _ => {}
-                }
-            }
-
-            if has_pending_recv && received.is_none() && pipe.can_resume_recv() {
-                match try!(pipe.recv()) {
-                    RecvStatus::Completed(msg) => received = Some((msg, pipe.token())),
-                    RecvStatus::InProgress     => receiving = Some(token),
-                    _ => {}
-                }
-            }
+            received = r;
         }
 
-        self.process_send_result(event_loop, sent, sending);
-        self.process_recv_result(event_loop, received, receiving);
+        let send_result = match sent {
+            true  => Ok(self.msg_sender.sent_by(event_loop, token, self.pipe.as_mut())),
+            false => self.msg_sender.resume_send(event_loop, token, self.pipe.as_mut())
+        };
 
-        Ok(())
+        let recv_result = match received {
+            Some(msg) => Ok(self.msg_receiver.received_by(event_loop, &mut self.codec, msg, token, self.pipe.as_mut())),
+            None => self.msg_receiver.resume_recv(event_loop, &mut self.codec, token, self.pipe.as_mut())
+        };
+
+        send_result.and(recv_result)
+    }
+}
+
+struct Codec {
+    backtrace: Vec<u8>,
+    ttl: u8
+}
+
+impl Codec {
+    fn new() -> Codec {
+        Codec {
+            backtrace: Vec::with_capacity(64),
+            ttl: 8
+        }
+    }
+
+    fn encode(&mut self, msg: Message) -> io::Result<(Message, mio::Token)> {
+        let (mut header, body) = msg.explode();
+        let pipe_id_bytes = vec!(
+            self.backtrace[0],
+            self.backtrace[1],
+            self.backtrace[2],
+            self.backtrace[3]
+        );
+        let mut pipe_id_reader = io::Cursor::new(pipe_id_bytes);
+        let pipe_id = try!(pipe_id_reader.read_u32::<BigEndian>());
+        let pipe_token = mio::Token(pipe_id as usize);
+
+        self.restore_saved_backtrace_to_header(&mut header);
+        self.backtrace.clear();
+
+        Ok((Message::with_header_and_body(header, body), pipe_token))
+    }
+
+    fn restore_saved_backtrace_to_header(&self, header: &mut Vec<u8>) {
+        let backtrace = &self.backtrace[4..];
+        
+        header.reserve(backtrace.len());
+        header.push_all(backtrace);
+    }
+
+    fn save_received_header_to_backtrace(&mut self, msg: &Message) {
+        self.backtrace.clear();
+        self.backtrace.push_all(msg.get_header());
+    }
+}
+
+impl MsgDecoder for Codec {
+    fn decode(&mut self, raw_msg: Message, pipe_token: mio::Token) -> io::Result<Option<Message>> {
+        let (mut header, mut body) = raw_msg.explode();
+        let pipe_id = pipe_token.as_usize() as u32;
+
+        header.reserve(4);
+        try!(header.write_u32::<BigEndian>(pipe_id));
+
+        let mut hops = 0;
+        loop {
+            if hops >= self.ttl {
+                return Ok(None);
+            }
+
+            hops += 1;
+
+            if body.len() < 4 {
+                return Ok(None);
+            }
+
+            let tail = body.split_off(4);
+            header.push_all(&body);
+
+            let position = header.len() - 4;
+            if header[position] & 0x80 != 0 {
+                let msg = Message::with_header_and_body(header, tail);
+
+                self.save_received_header_to_backtrace(&msg);
+
+                return Ok(Some(msg));
+            }
+            body = tail;
+        }
     }
 }
