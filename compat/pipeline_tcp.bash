b9e1098f0f3117e5b146046000192908b732645b
blabaere <benoit.labaere@gmail.com>
2016-04-11 08:08:45 +0200

    Updates compt test to be runnable from project dir

--- compat/pipeline_tcp.bash
+++ compat/pipeline_tcp.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 # 
 # TEST CASES
+COMPAT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+EXAMPLE_PATH="$( cd "$COMPAT_PATH/../target/debug/examples" ; pwd -P )"
 
 # TEST CASE 1
 msg="toto raoul simone"
-./target/debug/examples/td-pipeline node0 tcp://127.0.0.1:5454 > pipeline_tcp.log & node0=$!
+./target/debug/examples/td-pipeline node0 tcp://127.0.0.1:5454 > /tmp/pipeline_tc_1.log & node0=$!
 nanocat --push --connect tcp://127.0.0.1:5454 --data "$msg" > /dev/null -i2 & ncat=$!
 sleep 0.5 && kill $ncat && kill $node0
-result=`cat pipeline_tcp.log`
-expected=`cat expected.log`
+result=`cat /tmp/pipeline_tc_1.log`
+expected=`cat $COMPAT_PATH/pipeline_tc_1_expected.log`
 if [[ $result == $expected ]]; then
     echo "test case 1 SUCCEEDED !" 
 else
