291cfa8b0fb55db339fb4359cf39dfd0e7d86096
blabaere <benoit.labaere@gmail.com>
2016-08-30 23:15:19 +0200

    Removes extra licence header

--- test/fair_queue.rs
+++ test/fair_queue.rs
@@ -4,7 +4,6 @@
 // or the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // This file may not be copied, modified, or distributed except according to those terms.
 
-
 pub use scaproust::*;
 
 pub use super::urls;
