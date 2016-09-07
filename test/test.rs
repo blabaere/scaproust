c06a30cdcf25f4f2683055ecfb132ce951328564
blabaere <benoit.labaere@gmail.com>
2016-09-07 22:15:37 +0200

    Reduces test timeout back again, now that device seems to be fixed

--- test/test.rs
+++ test/test.rs
@@ -31,7 +31,7 @@ pub use env_logger::*;
 pub use scaproust::*;
 
 #[cfg(not(windows))]
-pub const SYS_TIMEOUT: u64 = 600;
+pub const SYS_TIMEOUT: u64 = 300;
 
 #[cfg(windows)]
 pub const SYS_TIMEOUT: u64 = 500;
