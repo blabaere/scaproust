28fb1bcf490507d1e5dfde7de952df887304eceb
Benoît Labaere <benoit.labaere@gmail.com>
2016-12-18 22:58:21 +0100

    Adds an implementation of ipc connect for windows

--- src/transport/ipc/windows/mod.rs
+++ src/transport/ipc/windows/mod.rs
@@ -24,6 +24,7 @@ use self::acceptor::IpcAcceptor;
 use transport::{Transport, Destination};
 use transport::pipe::Pipe;
 use transport::acceptor::Acceptor;
+use transport::async::AsyncPipe;
 use io_error::*;
 
 pub struct Ipc;
