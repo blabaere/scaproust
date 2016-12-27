dd771628925d6d0823fb046cf8214ea7ef3513b3
Benoît Labaere <benoit.labaere@gmail.com>
2016-12-27 14:47:47 +0100

    Adds a list of required packages to get kcov working

--- coverage.setup.bash
+++ coverage.setup.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# sudo apt-get install libdw-dev
+# sudo apt-get install elfutils-dev
+# sudo apt-get install elfutils
+# sudo apt-get install libiberty-dev
+# sudo apt-get install libelf-dev
+# sudo apt-get install binutils-dev
+# sudo apt-get install libssl-dev
+
 wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 tar xzf master.tar.gz
 rm master.tar.gz
